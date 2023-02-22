10cf7c8a5afad04aa495d38e154973907e5e66b2
Diwesh Chaurasia <110768034+dch2r@users.noreply.github.com>
2023-02-22 12:37:17 +0530

    Update code.cpp

--- Longest Substring without repeating characters/code.cpp
+++ Longest Substring without repeating characters/code.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int lengthOfLongestSubstring(String str) {
+        String test = "";
+        int maxlength = -1;
+        if (str.isEmpty()){
+            return 0;
+        }
+        else if(str.length()==1){
+            return 1;
+        }
+        for(char c: str.toCharArray()){
+            String current = String.valueOf(c);
+            if (test.contains(current)){
+                test = test.substring(test.indexOf(current)+1);
+            }
+            test=test + String.valueOf(c);
+            maxlength = Math.max(test.length(), maxlength);
+        }
+        return maxlength;
+    }
+};
