509970512e628a3ff0eb93d5893fc346dd25fd65
Diwesh Chaurasia <110768034+dch2r@users.noreply.github.com>
2023-02-23 21:54:59 +0530

    Create code.cpp

---
+++ 502. IPO/code.cpp
@@ -0,0 +1,27 @@
+using pi = pair<int,int>;
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        priority_queue<pi,vector<pi>, greater<pi>> q1;
+        int n = profits.size();
+        for(int i = 0;i<n;i++){
+            q1.push({
+                capital[i],profits[i]
+            });
+        }
+        priority_queue<int> q2;
+        while(k--){
+            while(!q1.empty()&&q1.top().first<=w){
+                q2.push(q1.top().second);
+                q1.pop();
+            }
+            if(q2.empty()){
+                break;
+            }
+            w += q2.top();
+            q2.pop();
+        }
+        return w;
+        
+    }
+};
