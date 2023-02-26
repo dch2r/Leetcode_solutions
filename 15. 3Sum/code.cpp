ef58f88dedff0fd7414ce091f8827f3b43de6a78
Diwesh Chaurasia <110768034+dch2r@users.noreply.github.com>
2023-02-26 19:49:15 +0530

    Create code.cpp

---
+++ 15. 3Sum/code.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector <vector<int>>
+        result;
+
+        sort(nums.begin(), nums.end());
+        for(int i =0; i<nums.size();i++){
+            if(i>0 && nums[i-1] == nums[i]){
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.size() -1;
+
+            while (j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum<0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    result.push_back({
+                        nums[i],nums[j],nums[k]
+                    });
+
+                    while((j+1)<nums.size() && nums[j]==nums[j+1]) j++;
+                    j++;
+                    while((k-1)>=0 && nums[k] ==nums[k-1]) k--;
+                    k--;
+                }
+            }
+        }
+        return result;
+    }
+};
